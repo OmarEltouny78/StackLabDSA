29e40c733b6223635ca0e31c0a2aa0345671b4c9
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2022-10-28 14:23:55 +0200

    Add files via upload

---
+++ StackTime/src/ArrayStack.java
@@ -0,0 +1,29 @@
+public class ArrayStack<E> implements Stack<E>{
+    // Variables size,top, data
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E top() {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+}
